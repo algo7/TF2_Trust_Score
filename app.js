f0b92f21ad82300e74aa8286bcd23f718cccd4a1
algo7 <11154774+algo7@users.noreply.github.com>
2021-12-13 19:47:04 +0100

    extract user name + get steam i func done

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
 // Dependencies
 const { default: axios } = require('axios');
 
+// Creds
+const { steamAPIKey } = require('./creds.json');
+
+// Steam profile url regex
+const steamProfileRegEx = /https:\/\/steamcommunity.com\/id\/*/
+// https://steamcommunity.com/id/Dr_Pepper_chemec/
+
 
 /**
- * Get the steam id from user's profile URL
- * @param {String} profileUrl - The user's profile URL
- * @returns {String} - The user's steam id
+ * Get the user name from the user's profile url
+ * @param {String} profileUrl - The user's profile url
+ * @returns {String} - The user's name
  */
-const getSteamId = async (profileUrl) => {
+const extractUserName = (profileUrl) => {
     try {
+        // Check if the profile url is valid
+        if (!steamProfileRegEx.test(profileUrl)) {
+            throw ('Invalid profile URL');
+        }
+        // Extract the user name
+        const userName = profileUrl.split('/')[4];
+
+        return userName;
 
     } catch (err) {
         console.log(err);
     }
 }
 
+/**
+ * Get the steam id from the user name
+ * @param {String} userName - The user name
+ * @returns {String} - The user's steam id
+ */
+const getSteamId = async (userName) => {
+    try {
 
-// http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=YOU-API_KEY&vanityurl=USER_NAME
+        // The request config
+        const requestConfig = {
+            url: `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamAPIKey}&vanityurl=${userName}`,
+            method: 'GET',
+        }
+
+        // Make the request
+        const { data: { response: { steamid } } } = await axios(requestConfig);
+
+        return steamid;
+
+    } catch (err) {
+        console.log(err.toJSON());
+    }
+}
+getSteamId(extractUserName('https://steamcommunity.com/id/Dr_Pepper_chemec/'));
