1e5a482887435ac8d4f1ffc08c64de922ee21463
algo7 <11154774+algo7@users.noreply.github.com>
2021-12-13 19:32:26 +0100

    prof url func

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // Dependencies
 const { default: axios } = require('axios');
+
+
+/**
+ * Get the steam id from user's profile URL
+ * @param {String} profileUrl - The user's profile URL
+ * @returns {String} - The user's steam id
+ */
+const getSteamId = async (profileUrl) => {
+    try {
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+// http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=YOU-API_KEY&vanityurl=USER_NAME
