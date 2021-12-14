d5e5c356a51faab16acbe4716c2bfc062c29c46a
algo7 <11154774+algo7@users.noreply.github.com>
2021-12-14 12:27:21 +0100

    add asyncHandler example

--- controllers/main.js
+++ controllers/main.js
@@ -4,9 +4,14 @@ const asyncHandler = require('../libs/asyncHandler');
 // @desc Allow the admin to get all the reported conversations
 // @route DELETE /admin/interest/delete
 // @access Private
-const adminDeletePassion = asyncHandler(async (req, res) => {
+const adminDeletePassion = asyncHandler(async (req, res, next) => {
 
     res.status(200).json({ msg: 'ok', });
+
+    if (!1) {
+        return next('x');
+    }
+
 });
 
 module.exports = adminDeletePassion;
