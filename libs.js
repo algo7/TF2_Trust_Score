534e54cc1ac15f058bfb046c7ab3128f249d0081
algo7 <11154774+algo7@users.noreply.github.com>
2021-12-14 10:08:28 +0100

    add sample data

--- libs.js
+++ libs.js
@@ -510,7 +510,7 @@ getSteamId('https://steamcommunity.com/id/avivlo0612/')
 
     }).catch(err => console.log(err));
 
-trustFactorDataPreprocessing('https://steamcommunity.com/id/heinrichderpro/')
+trustFactorDataPreprocessing('https://steamcommunity.com/profiles/76561198962558280/')
     .then(data => console.log(data))
     .catch(err => console.log(err));
 
@@ -522,6 +522,20 @@ trustFactorDataPreprocessing('https://steamcommunity.com/id/heinrichderpro/')
  */
 const trustFactor = (info) => {
     try {
+        // const a = {
+        //     timeSinceCreation: 1709,
+        //     profileVsibility: true,
+        //     steamLevel: 10,
+        //     gameCount: 15,
+        //     friendVACBanPercentage: 0.08771929824561403,
+        //     commentSentimentScore: 0.05328269126424308,
+        //     totalHours: 4447.266666666666,
+        //     totalHoursLinux: 41,
+        //     totalHoursLinuxPercentage: 0.009436507817535864,
+        //     friendCount: 285,
+        // };
+
+        // { profileVsibility: false, timeSinceCreation: 0, steamLevel: 0 }
         console.log(info);
     } catch (err) {
         throw err;
