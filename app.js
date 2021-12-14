078ab6ecccd9200a8fbbb261874667620bdef7c2
algo7 <11154774+algo7@users.noreply.github.com>
2021-12-14 12:14:50 +0100

    add express

---
+++ app.js
@@ -0,0 +1,51 @@
+// Dependencies
+const express = require('express');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const { routeCheck, } = require('express-suite');
+
+// Global variables
+const PORT = process.env.PORT || 9000;
+
+// Initialize the App
+const app = express();
+
+// Compression Module
+app.use(compression({ level: 9, memLevel: 9, }));
+
+// Disable etag
+app.set('etag', false);
+app.set('x-powered-by', false);
+
+// Express parser Middleware
+app.use(express.urlencoded({
+    extended: true,
+    limit: '5mb',
+}));
+
+app.use(express.json({
+    limit: '5mb',
+    extended: true,
+}));
+
+app.use(express.text({
+    limit: '5mb',
+    extended: true,
+}));
+
+// Cookie parser
+app.use(cookieParser());
+
+/**
+ * All routes go here
+ */
+// Route Check
+app.use(routeCheck(app));
+
+// Start the app
+app.listen(PORT, () => {
+    console.info(`Server is listening in ${process.env.NODE_ENV} on port ${PORT}`);
+});
+
+
+
